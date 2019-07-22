348f379a9ed1275d43f1859ff1581497d8a90dc2
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-07-22 15:31:42 +0530

    scopes

--- 1-JS-level-1/1-data-types.js
+++ 1-JS-level-1/1-data-types.js
@@ -1,4 +1,4 @@
-
+"use strict"
 
 /**
  * 
@@ -91,24 +91,66 @@ var isFound = true;
 //-----------------------------------------------------
 // 1. complex / reference  ==> object
 //-----------------------------------------------------
+//-------------------------------------------
+// class / constructor  : till ES5
+//-------------------------------------------
 
-// class / constructor
+/* 
 function Person(name, age) {
     this.name = name;
     this.age = age;
-    // this.sayName = function () {
-    //     console.log("im " + this.name);
-    // }
-    // this.sayAge = function () {
-    //     console.log("im " + this.age + " old")
-    // }
 }
 Person.prototype.sayName = function () {
     console.log("im " + this.name);
 }
 Person.prototype.sayAge = function () {
     console.log("im " + this.age + " old")
+} 
+*/
+
+//-------------------------------------------
+// class / constructor  : From ES6
+//-------------------------------------------
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    sayName() {
+        console.log("im " + this.name);
+    }
+    sayAge() {
+        console.log("im " + this.age + " old");
+    }
 }
+
+// var p1 = new Person("Nag", 36)
+// var p2 = new Person("Ria", 4)
+
+//-------------------------------------------
+
+/**
+ * im-note:
+ * 
+ * by default , all javascript objects are extensible & configurable & modifiable
+ *              i.e we add/delete/change properties on demand 
+ * 
+ */
+
 
 var p1 = new Person("Nag", 36)
 var p2 = new Person("Ria", 4)
+
+console.log(Object.isExtensible(p1)) // true
+console.log(Object.isSealed(p1)) // false
+console.log(Object.isFrozen()) // false
+
+// Object.preventExtensions(p1);
+// Object.seal(p1);  + // Object.preventExtensions(p1);
+// Object.freeze(p1); +  // Object.seal(p1); 
+
+// p1.teachSkill = "JS"  // Add New property
+// delete p1.name;       // delete e xistin property
+// p1.name="Nil"         // change/modify  
+
