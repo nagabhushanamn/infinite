b447e2987effa543feaac5caa2e3adcd8418df0a
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-07-22 16:42:34 +0530

    FP intro

---
+++ 1-JS-level-1/Ex1.js
@@ -0,0 +1,25 @@
+
+
+// Ex1.
+function f1() {
+    console.log("before :: LOG")
+    console.log("f1()")
+    console.log("after :: LOG")
+}
+function f2() {
+    console.log("before :: LOG")
+    console.log("f2()")
+    console.log("after :: LOG")
+}
+/**
+ *
+ * design issues
+ *
+ *  => code tangling / tight-coupling
+ *  => code scattering / duplication
+ *
+ * soln ?
+ *
+ *
+ */
+
